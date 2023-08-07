ba23b34ad8b3cac1e693790b711513aae940d66f
Daniel Gift <daniel.gift@sbs.org>
2023-08-07 15:03:13 -0400

    Create StdDev.java

---
+++ StdDev.java
@@ -0,0 +1,19 @@
+public class StdDev{
+  public static double sum(int x1, int x2, int x3){
+  }
+  
+  public static double average(int x1, int x2, int x3){
+  }
+  
+  public static double variance(int x1, int x2, int x3){
+  }
+  
+  public static double stdDev(int x1, int x2, int x3){
+  }
+  
+  public static void main(String[] args){
+    int x = 78; int y = 80; int z = 77;
+    //Print out the sum, average, variance, and std dev of the above 3 ints
+    
+    //Be sure to write at least one additional test !
+  }
